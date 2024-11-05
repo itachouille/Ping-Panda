b709a54c2547cddc3edae47e68ec06f5e2c41a5e
Anthony David <anthony.david34@sfr.fr>
2024-11-05 16:01:43 +0100

    dashboard page content

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,25 @@
-const DashboardPage = () => {
-  return <div>DashboardPage</div>
+import DashboardPageLayout from "@/components/DashboardPageLayout"
+import { db } from "@/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import DasboardPageContent from "./_components/DasboardPageContent"
+
+const DashboardPage = async () => {
+  const auth = await currentUser()
+
+  if (!auth) redirect("/sign-up")
+
+  const user = await db.user.findUnique({
+    where: { externalId: auth.id },
+  })
+
+  if (!user) redirect("/sign-in")
+
+  return (
+    <DashboardPageLayout title="Dashboard">
+      <DasboardPageContent />
+    </DashboardPageLayout>
+  )
 }
 
 export default DashboardPage
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono"
 import { cors } from "hono/cors"
 import { handle } from "hono/vercel"
 import { authRouter } from "./routers/auth-router"
-//import { categoryRouter } from "./routers/category-router"
+import { categoryRouter } from "./routers/category-router"
 //import { paymentRouter } from "./routers/payment-router"
 //import { projectRouter } from "./routers/project-router"
 
@@ -13,9 +13,10 @@ const app = new Hono().basePath("/api").use(cors())
  *
  * All routers added in /server/routers should be manually added here.
  */
-const appRouter = app.route("/auth", authRouter)
-/*  .route("/category", categoryRouter)
-  .route("/payment", paymentRouter)
+const appRouter = app
+  .route("/auth", authRouter)
+  .route("/category", categoryRouter)
+/* .route("/payment", paymentRouter)
   .route("/project", projectRouter)
  */
 // The handler Next.js uses to answer API requests
