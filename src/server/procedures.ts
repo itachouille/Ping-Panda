9fa51349e90da28206098a805857da2dac35a4ea
Anthony David <anthony.david34@sfr.fr>
2024-11-05 12:06:39 +0100

    auth route / create user

--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,4 +1,10 @@
 import { j } from "./__internals/j"
 
+const authMiddleware = j.middleware(({ next }) => {
+  return next({})
+})
+
 export const baseProcedure = j.procedure
 export const publicProcedure = baseProcedure
+
+export const privateProcedure = publicProcedure.use(authMiddleware)
